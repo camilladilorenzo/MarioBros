1a646db19f1603747e35fb0a4bc3ed41adfcf637
Camilla Di Lorenzo <camilla.dilorenzo2@gmail.com>
2022-05-25 17:46:20 +0200

    - FILE MARIOBROS TEST MODIFIED WITH THE NEW WAY OF IMPORTING THE MODEL - FILE XRL WITH LIME THAT RUN

--- MarioBros_Test.py
+++ MarioBros_Test.py
@@ -1,14 +1,20 @@
 import pickle
-import time
+import time, datetime
 from MarioBros import prepare_env
-from MarioBros import Mario, MarioNet
+from MarioBros import Mario, MarioNet, MetricLogger
 import copy
+import torch
+from pathlib import Path
+
 
 if __name__ == '__main__':
-    with open("mariosave.pkl", "rb") as f_in:
-        mario = pickle.load(f_in)
-
     env = prepare_env()
+
+    marionet_file = 'marionetsave.pt'
+    marionet = MarioNet((4, 84, 84), env.action_space.n).float()
+    marionet.load_state_dict(torch.load(marionet_file, map_location=torch.device('cpu'))['model'])
+    marionet.eval()
+
     frames = []
     episodes = 1
     for e in range(episodes):
@@ -17,13 +23,17 @@ if __name__ == '__main__':
         state = env.reset()
         while not done:
             # Run agent on the state
-            action = mario.act(state)
+            state = torch.tensor(state.__array__()).unsqueeze(0)
+            net_evaluation = marionet(state, model="online")
+            action = torch.argmax(net_evaluation).item()
+            print(net_evaluation.tolist(), " -> ", action)
+
             frame = env.render(mode='rgb_array')
             frames.append(copy.deepcopy(frame))
 
             # Agent performs action
             next_state, reward, done, info = env.step(action)
-            time.sleep(0.05)
+            time.sleep(0.04)
 
             # Update state
             state = next_state
@@ -33,3 +43,5 @@ if __name__ == '__main__':
 
     with open("frame_img.pkl", "wb") as frames_out:
         pickle.dump(frames, frames_out)
+
+
--- MarioBros_XRL.py
+++ MarioBros_XRL.py
@@ -5,12 +5,18 @@ from matplotlib import pyplot as plt
 import torch
 from torchvision import transforms as T
 from MarioBros import Mario, MarioNet, prepare_env
+from skimage.segmentation import mark_boundaries
+from lime import lime_image
 
 if __name__ == '__main__':
     with open("frame_img.pkl", "rb") as frames_in:
         frames_images = pickle.load(frames_in)
-    with open("mariosave.pkl", "rb") as f_in:
-        mario = pickle.load(f_in)
+
+    env = prepare_env()
+    marionet_file = 'marionetsave.pt'
+    marionet = MarioNet((4, 84, 84), env.action_space.n).float()
+    marionet.load_state_dict(torch.load(marionet_file, map_location=torch.device('cpu'))['model'])
+    marionet.eval()
 
     transforms = T.Compose(
         # resize image in seLf.shape dimensions and then normalize image
@@ -48,13 +54,12 @@ if __name__ == '__main__':
 
     act = []
 
-    net = mario.net.double()
+    net = marionet.double()
     for i in range(4,len(frames_images)):
         state = torch.stack((mod_frames[i - 0], mod_frames[i - 1], mod_frames[i - 2], mod_frames[i - 3]))
         state = state.squeeze(1) # 4 3 2 1
         state = state.unsqueeze(0)
         act.append(net(state, model="online"))
-
 
     labels_actions = []
     for i in range(len(act)):
@@ -75,15 +80,15 @@ if __name__ == '__main__':
     fig = plt.figure(figsize=(20,20))
 
     # setting values to rows and column variables
-    rows = 8
-    columns = 8
+    rows = 5
+    columns = 5
 
     pil_im = []
     for i in range(4,len(frames_images)):
         # reading images
         pil_im.append(Image.fromarray(frames_images[i]))
 
-    for i in range(64):
+    for i in range(20):
         # Adds a subplot at the 1st position
         fig.add_subplot(rows, columns, i + 1)
 
@@ -97,12 +102,60 @@ if __name__ == '__main__':
     plt.margins(0, 0)
     fig.show()
 
-    # plt.imshow(pil_im[0])
-    # from lime import lime_image
-    #
-    #
-    # state_lime = torch.stack((mod_frames[29], mod_frames[28], mod_frames[27], mod_frames[26]))
-    # state_lime = state_lime.squeeze(1)
-    # state_lime = state_lime.unsqueeze(0)
-    # explainer = lime_image.LimeImageExplainer()
-    # explaination = explainer.explain_instance(frames_images[27], net, top_labels=5, hide_color=0, num_samples=1000)
+    plt.imshow(pil_im[0])
+
+
+    # LIME IMPLEMENTATION
+    def concatenate_img(im1, im2, im3, im4, plot_flag = False):
+    # CONCATENATE IMAGES (OK PER INPUT)
+        vertical = np.concatenate((im4, im2), axis=0)
+        vertical1 = np.concatenate((im3, im1), axis=0)
+        horiz = np.concatenate((vertical, vertical1), axis=1)
+        if plot_flag == True:
+            Image.fromarray(horiz).show()
+        return horiz
+
+    def separate_img(image):
+        # SEPARATE IMAGES
+        hsep = np.hsplit(image, [256])
+        hsep0 = hsep[0]
+        hsep1 = hsep[1]
+        vsep0 = np.vsplit(hsep0, [240])
+        vsep1 = np.vsplit(hsep1, [240])
+        images = [vsep0[0], vsep1[0],vsep0[1],vsep1[1]]
+        return images
+
+    def image_to_state(list_img):
+        four_frames = []
+        for i in range(4):
+            img = list_img[i]
+            img = np.resize(img, (1, 256, 512, 3))
+            img = img.squeeze(0)
+            img = np.transpose(np.asarray(img), (2, 0, 1))
+            img = torch.tensor(img, dtype=torch.float64)
+            bw_transform = T.Grayscale()
+            img = bw_transform(img)
+            img = transforms(img)
+            four_frames.append(img)
+        state_lime = torch.stack((four_frames[0], four_frames[1], four_frames[2], four_frames[3]))
+        state_lime = state_lime.squeeze(1)
+        state_lime = state_lime.unsqueeze(0)
+        state_lime = state_lime.expand(10, -1,-1,-1)
+        return state_lime
+
+    concat_im = concatenate_img(frames_images[26], frames_images[27], frames_images[28], frames_images[29])
+    def net_lime(image):
+        list_images = separate_img(image)
+        state_lime = image_to_state(list_images)
+        return net(state_lime, model = 'online').detach().numpy()
+
+
+    explainer = lime_image.LimeImageExplainer()
+    explanation = explainer.explain_instance(concat_im, net_lime, top_labels=3, hide_color=0, num_samples=1000)
+
+    temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=False, num_features=10,
+                                                hide_rest=False)
+
+    # NON SO PERCHE NON MOSTRA IMMAGINE
+    img_boundry2 = mark_boundaries(temp / 255.0, mask)
+    plt.imshow(img_boundry2)
